96d157b702e86fc38dc3467abe05d2b81f648949
Per Jansson <per.r.jansson@gmail.com>
2017-11-13 23:53:16 +0100

    responsive headers

--- components/layout.js
+++ components/layout.js
@@ -23,17 +23,32 @@ const Layout = ({ renderHeader, children }) => [
           color: #fff;
           text-shadow: 6px 6px 12px #000;
           opacity: 0.5;
-          font-size: 36px;
+          font-size: 14px;
           white-space: nowrap;
+          text-overflow: hidden;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
         }
 
-        @media all and (min-width: 960px) {
+        @media all and (min-width: 600px) {
           .header {
-            font-size: 64px;
+            font-size: 20px;
+            margin-top: -10px;
+          }
+        }
+
+        @media all and (min-width: 750px) {
+          .header {
+            font-size: 32px;
+            margin-top: -20px;
+          }
+        }
+
+        @media all and (min-width: 1200px) {
+          .header {
+            font-size: 44px;
             margin-top: -50px;
           }
         }
