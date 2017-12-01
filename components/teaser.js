d3971ea4f0c0e2052ab0de6941633bc1413552ec
Per Jansson <per.r.jansson@gmail.com>
2017-12-01 16:04:47 +0100

    Webp teaser images for browser that supports it

--- components/teaser.js
+++ components/teaser.js
@@ -9,36 +9,48 @@ const Teaser = ({ burger }) => (
           height: 100%;
           background-blend-mode: multiply;
           filter: contrast(120%) saturate(125%);
-          background-image: linear-gradient(rgba(195, 198, 0, 0), rgba(195, 198, 0, 0.3)),
-              url('/static/images/burgers/teaser/${burger.id}.jpg');
           background-size: cover;
+        }
 
+        :global(.webp) {
+          .teaser {
+            background-image: linear-gradient(rgba(195, 198, 0, 0), rgba(195, 198, 0, 0.3)),
+              url('/static/images/burgers/teaser/${burger.id}.webp');
+          }
+        }
+
+        :global(.no-webp) {
+          .teaser {
+            background-image: linear-gradient(rgba(195, 198, 0, 0), rgba(195, 198, 0, 0.3)),
+              url('/static/images/burgers/teaser/${burger.id}.jpg');
+          }
+        }
+
+        .burger-name {
+          margin: 0;
+          padding: 20px;
+          color: #fff;
+          text-shadow: 3px 3px 6px #000;
+          opacity: 0.8;
+          text-transform: uppercase;
+          font-size: 34px;
+        }
+
+        @media all and (min-width: 600px) {
           .burger-name {
-            margin: 0;
-            padding: 20px;
-            color: #fff;
-            text-shadow: 3px 3px 6px #000;
-            opacity: 0.8;
-            text-transform: uppercase;
-            font-size: 34px;
+            font-size: 28px;
           }
+        }
 
-          @media all and (min-width: 600px) {
-            .burger-name {
-              font-size: 28px;
-            }
+        @media all and (min-width: 750px) {
+          .burger-name {
+            font-size: 48px;
           }
+        }
 
-          @media all and (min-width: 750px) {
-            .burger-name {
-              font-size: 48px;
-            }
-          }
-
-          @media all and (min-width: 1200px) {
-            .burger-name {
-              font-size: 42px;
-            }
+        @media all and (min-width: 1200px) {
+          .burger-name {
+            font-size: 42px;
           }
         }
       `}
