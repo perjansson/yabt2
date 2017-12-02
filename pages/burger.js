def2ef2cc9a37efa78107affeadf149c87a2be27
Per Jansson <per.r.jansson@gmail.com>
2017-12-02 21:26:22 +0100

    progressive loading bun meat bun

--- pages/burger.js
+++ pages/burger.js
@@ -1,37 +1,65 @@
+import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import fetch from 'isomorphic-unfetch'
+import ProgressiveImage from 'react-progressive-image'
 
 import Layout from '../components/layout'
+import inline from '../components/inline'
 
 import apiUrl from '../config/apiUrl'
+
+const filter = loading => ({
+  filter: `contrast(130%) saturate(150%) ${loading ? 'blur(25px)' : ''}`,
+})
 
 const Burger = ({ id, name, web }) => (
   <Layout
     key="body"
-    renderHeader={() => [
-      <h1 key="burger-name">{name}</h1>,
-      <a key="web-page" href={web} target="_blank">
-        &gt; Homepage &lt;
-      </a>,
-    ]}
+    renderHeader={() => (
+      <Fragment>
+        <h1 key="burger-name">{name}</h1>
+        <a key="web-page" href={web} target="_blank">
+          &gt; Homepage &lt;
+        </a>
+      </Fragment>
+    )}
   >
-    <div className="burger" />
+    <div className="wrapper">
+      <ProgressiveImage src={`/static/images/burgers/${id}.jpg`} placeholder={inline[id]}>
+        {(src, loading) => (
+          <img
+            style={filter(loading)}
+            className="burger-image"
+            src={src}
+            alt={`Burger from ${name}`}
+          />
+        )}
+      </ProgressiveImage>
+    </div>
     <style jsx>
       {`
-        .burger {
+        .wrapper {
+          display: inline-block;
+          width: 100%;
           height: 100%;
+          background-repeat: no-repeat;
+          background: linear-gradient(
+            to bottom,
+            transparent,
+            transparent,
+            transparent,
+            rgba(0, 0, 0, 0.75)
+          );
+        }
+
+        .burger-image {
+          position: relative;
+          z-index: -1;
+          display: block;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
           background-blend-mode: multiply;
-          filter: contrast(120%) saturate(125%);
-          background-image: 
-            linear-gradient(
-              to bottom,
-              transparent,
-              transparent,
-              transparent,
-              rgba(0,0,0,0.75)
-            ),
-            url('/static/images/burgers/${id}.jpg');
-          background-size: cover;
         }
       `}
     </style>
